0f8c695aecbfd27643cda0f2a849d8e6b71f95cc
Da504 <dam342296@gmail.com>
2025-03-01 09:12:19 -0600

    terminando el ejercicio 1

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -56,8 +56,64 @@ public class Ejercicio1 {
         System.out.println("Total de importación: $" + totalImportacion);
 
 
-
-
-
     }
+    private static double calcularImpuesto(int añoFabricacion, double valorVehiculo) {
+        int añosAntiguedad = 2025 - añoFabricacion;
+
+        if (añosAntiguedad >= 10 && añosAntiguedad <= 15) {
+            return valorVehiculo * 0.16;
+        } else if (añosAntiguedad >= 5 && añosAntiguedad <= 9) {
+            return valorVehiculo * 0.13;
+        } else if (añosAntiguedad >= 1 && añosAntiguedad <= 4) {
+            return valorVehiculo * 0.11;
+        } else {
+            return 0;
+        }
+    }
+
+    private static double calcularFlete(String paisOrigen, String tipoVehiculo) {
+        switch (paisOrigen.toLowerCase()) {
+            case "china":
+                switch (tipoVehiculo.toLowerCase()) {
+                    case "sedan":
+                        return 1700.00;
+                    case "pickups":
+                        return 1900.00;
+                    case "microbuses":
+                        return 2400.00;
+                    case "motos":
+                        return 1300.00;
+                    default:
+                        return 0;
+                }
+            case "estados unidos":
+                switch (tipoVehiculo.toLowerCase()) {
+                    case "sedan":
+                        return 1200.00;
+                    case "pickups":
+                        return 1500.00;
+                    case "microbuses":
+                        return 1700.00;
+                    case "motos":
+                        return 900.00;
+                    default:
+                        return 0;
+                }
+            default:
+                return 0;
+        }
+    }
+
+    private static double calcularMatricula(int añoFabricacion) {
+        if (añoFabricacion >= 2020 && añoFabricacion <= 2025) {
+            return 11.99;
+        } else if (añoFabricacion >= 2016 && añoFabricacion <= 2019) {
+            return 13.99;
+        } else if (añoFabricacion >= 2010 && añoFabricacion <= 2015) {
+            return 15.99;
+        } else {
+            return 0;
+        }
+    }
+
 }
